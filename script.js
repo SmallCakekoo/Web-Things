06480be5df112d5f7af44da41f6a05a1042aeb33
SmallCakekoo <payan.sary@gmail.com>
2026-02-02 19:09:55 -0500

    first commit

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const todoList = document.getElementById('todoList');
 
 // It's a li into the ul item(?)
 addButton.addEventListener('click', () => {
-    const todoText = todoInput.value.trim();
-    if (todoText !== '') {
-        const li = document.createElement('li');
+    const todoText = todoInput.value.trim(); // the trim is for removing the spaces
+    if (todoText !== '') { // if the todoText is not empty
+        const li = document.createElement('li'); // create a li
         li.textContent = todoText + ' '; 
         const liDeleteButton = document.createElement('button');
-        liDeleteButton.textContent = 'Delete';
-        liDeleteButton.onclick = () => {
-            todoList.removeChild(li);
+        liDeleteButton.textContent = 'Delete'; // the delete button
+        liDeleteButton.onclick = () => { 
+            todoList.removeChild(li); // remove the li from the ul
         };
 
-        li.appendChild(liDeleteButton);
+        li.appendChild(liDeleteButton); // add the delete button to the li
         todoList.appendChild(li);
         todoInput.value = '';
     }

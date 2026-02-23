8b22f04b1181d3d4ec2566e3167c731333ca7ff0
SmallCakekoo <payan.sary@gmail.com>
2026-02-23 18:59:05 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Form } from './components/Form/Form';
+import { ShoppingCart } from './components/ShoppingCart';
 import './App.css';
 
 function App() {
   return (
     <>
-    <h1>Welcome to the To Do List App</h1> 
-    <Form />
+      <h1>Shopping Cat App 𐔌՞ ܸ.ˬ.ܸ՞𐦯</h1>
+      <ShoppingCart />
     </>
   );
 }

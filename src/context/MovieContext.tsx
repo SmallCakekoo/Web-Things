17638289c5ec5ad0bc126d5ecb02623aaebea368
SmallCakekoo <payan.sary@gmail.com>
2026-04-13 23:55:23 -0500

    .

--- src/context/MovieContext.tsx
+++ src/context/MovieContext.tsx
@@ -10,6 +10,7 @@ export interface Movie {
 }
 
 // Tipado del contexto
+// El dispath es el tipo de función que actualiza el estado, en este caso un array de películas
 interface MovieContextType {
   favorites: Movie[];
   setFavorites: React.Dispatch<React.SetStateAction<Movie[]>>;
@@ -17,7 +18,7 @@ interface MovieContextType {
   setWatchLater: React.Dispatch<React.SetStateAction<Movie[]>>;
 }
 
-// Contexto inicial en null (como te piden)
+// Contexto inicial en null
 const MovieContext = createContext<MovieContextType | null>(null);
 
 // Provider

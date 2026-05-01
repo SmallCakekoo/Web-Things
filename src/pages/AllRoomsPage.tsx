94fa284bc63955180314f92bfb7745ff2904a4a4
SmallCakekoo <payan.sary@gmail.com>
2026-04-30 23:56:03 -0500

    .

--- src/pages/AllRoomsPage.tsx
+++ src/pages/AllRoomsPage.tsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { RoomsContext } from "../context/RoomsContext";
 import type { RoomsContextType } from "../context/RoomsContext";
-import type { Room } from "../context/RoomsContext";
 import { FiltersBar } from "../components/FiltersBar";
 import { RoomCard } from "../components/RoomCard";
 
